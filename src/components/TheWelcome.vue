6527b6bec8d7e1dc4cd9d22bd00f8c23e75ce1de
retha cindy aulia <127622712+Rethacindyaulia@users.noreply.github.com>
2024-05-05 21:37:50 +0700

    Add files via upload

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- Elemen input teks -->
+    <input type="text" v-model="inputText" @input="handleInput" :class="{ 'red-text': inputText.length > 10 }" :style="{ 'font-weight': inputText.length > 10 ? 'bold' : 'normal' }">
+    <p v-if="inputText.length > 0">Tulisan Anda: {{ inputText }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+// Mendefinisikan variabel reaktif untuk menyimpan nilai input teks
+const inputText = ref('');
+
+// Event handler untuk memperbarui nilai inputText saat input diubah
+const handleInput = (event) => {
+  inputText.value = event.target.value;
+};
+</script>
+
+<style scoped>
+.red-text {
+  color: red;
+}
+</style>
